11bc90bd0b969f5b8eb2b615c89ce6e048f7326c
robbradyire <robbradyire@gmail.com>
2015-02-15 18:29:48 +0000

    Made process_item() take a (sorta) random amount of time and added a consume() and produce() method.

--- lab2/main.c
+++ lab2/main.c
@@ -10,15 +10,14 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define false 0
-#define true !false
-#define jobsize 1000000000
+#define true 1
 #define pipe_size 10
 
 int is_full = false;
 int is_empty = true;
+int num_pipe_items = 0;
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -27,13 +26,53 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 	a method that "processes" an item from the pipeline
 */
 void process_item() {
+	int jobsize = rand()/2;
 	int i = 0;
 	while (i < jobsize) {
 		i++;
 	}
 }
 
+void * consume(void * arg) {
+	while (true) {
+		while (is_empty == true) {
+			pthread_cond_wait(&cond, &lock);
+		}
+		pthread_mutex_lock(&lock);
+		num_pipe_items--;
+		if (num_pipe_items == 0) {
+			is_empty = true;
+		}
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&lock);
+		process_item();
+	}
+}
+
+void * produce(void * arg) {
+	while (true) {
+		while (is_full) {
+			pthread_cond_wait(&cond, &lock);
+		}
+		pthread_mutex_lock(&lock);
+		num_pipe_items++;
+		if (num_pipe_items == pipe_size) {
+			is_full = true;
+		}
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&lock);
+	}
+}
+
 int main() {
+	process_item();
 	process_item();
 	return 0;
 }
+
+
+
+
+
+
+
