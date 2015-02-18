eed7657620b8ad311ed96401cf2594955daa136d
robbradyire <robbradyire@gmail.com>
2015-02-18 09:48:38 +0000

    The program now ends when it's done. Still have to find a reliable way to end the consumer threads after the producers are done but for the moment the programs doing what it's supposed to.

--- lab2/main.c
+++ lab2/main.c
@@ -18,6 +18,9 @@
 int is_full = false;
 int is_empty = false;
 int num_pipe_items = 2;
+int items_left = true;
+int total_items = 20;
+int items_per_thread;
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t full_cond = PTHREAD_COND_INITIALIZER;
@@ -47,8 +50,8 @@ void * consume(void * arg) {
 	int rc;
 
 	pthread_mutex_lock(&lock);
-	while (true) {
-		while (is_empty) {
+	while (items_left) {
+		while (is_empty && items_left) {
 			rc = pthread_cond_wait(&empty_cond, &lock);
 			if (rc) {
 				pthread_mutex_unlock(&lock);
@@ -62,16 +65,20 @@ void * consume(void * arg) {
 		print_buffer(num_pipe_items);
 		pthread_mutex_unlock(&lock);
 		pthread_cond_signal(&full_cond);
+		pthread_cond_signal(&empty_cond);
 		process_item();
 	}
+	printf("Consumer done.\n");
+	pthread_exit(NULL);
 }
 
 void * produce(void * arg) {
 	int rc;
+	int items_to_make = items_per_thread;
 
 	pthread_mutex_lock(&lock);
-	while (true) {
-		while (is_full) {
+	while (items_to_make--) {
+		while (is_full && items_to_make) {
 			rc = pthread_cond_wait(&full_cond, &lock);
 			if (rc) {
 				pthread_mutex_unlock(&lock);
@@ -87,13 +94,16 @@ void * produce(void * arg) {
 		pthread_cond_signal(&empty_cond);
 		process_item();
 	}
+	printf("Producer done.\n");
+	pthread_exit(NULL);
 }
 
 int main() {
-	int n_threads = 5;
+	int n_threads = 8;
 	pthread_t producers[n_threads];
 	pthread_t consumers[n_threads];
 	int i, err1, err2;
+	items_per_thread = total_items / n_threads;
 
 	for (i = 0; i < n_threads; i++) {
 		err2 = pthread_create(&(consumers[i]), NULL, consume, NULL);
@@ -105,7 +115,14 @@ int main() {
 	}
 
 	for (i = 0; i < n_threads; i++) {
-		pthread_join(&(consumers[i]), NULL);
-		pthread_join(&(producers[i]), NULL);
+		pthread_join(producers[i], NULL);
 	}
+
+	fprintf(stderr, "items_left is false.\n");
+	items_left = false;
+	pthread_cond_broadcast(&empty_cond);
+	for (i = 0; i < n_threads; i++) {
+		pthread_join(consumers[i], NULL);
+	}
+	return 0;
 }
